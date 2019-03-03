f4e869588b319565625d5cb1dc6759e6eca9ef6d
Wojciech <wojciech.morkowski@gmail.com>
2019-03-03 21:53:01 +0100

    multibitrate example for linux

--- encode.sh
+++ encode.sh
@@ -3,15 +3,19 @@
 ffmpeg \
     -f x11grab \
     -r 30 \
-    -video_size 1024x768 \
     -i $DISPLAY \
+    -map 0 \
+    -map 0 \
+    -b:v:0 1000k \
+    -b:v:1 256k \
+    -s:1 400x300 \
     -codec:v libx264 \
     -profile:v baseline \
     -level 4 \
     -pix_fmt yuv420p \
     -preset veryfast \
-    -codec:a aac \
     -f dash \
     -window_size 5 \
     -remove_at_exit 1 \
+    -adaptation_sets "id=0,streams=0 id=1,streams=1" \
     public/media/live.mpd
